cac887dd5af94229ec23df2d026d7e8f0d437337
littlewin <littlewin.wang@gmail.com>
2017-08-19 10:26:27 +0800

    feature: update sidebar width

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,22 +1,14 @@
 <template>
-  <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+  <el-menu default-active="1-4-1" theme="dark" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+    <!--<div class="avatar"></div>-->
     <el-submenu index="1">
       <template slot="title">
         <i class="el-icon-message"></i>
         <span slot="title">导航一</span>
       </template>
-      <el-menu-item-group>
-        <span slot="title">分组一</span>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <span slot="title">选项4</span>
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
-      </el-submenu>
+      <el-menu-item index="1-1">选项1</el-menu-item>
+      <el-menu-item index="1-2">选项2</el-menu-item>
+      <el-menu-item index="1-4-1">选项1</el-menu-item>
     </el-submenu>
     <el-menu-item index="2">
       <i class="el-icon-menu"></i>
@@ -57,3 +49,13 @@
 //    }
   }
 </script>
+
+<style lang="stylus" scoped>
+  .avatar
+    width: 100%
+    height: 200px
+    background: yellow
+
+  .el-menu:not(.el-menu--collapse)
+    min-width: 200px
+</style>
